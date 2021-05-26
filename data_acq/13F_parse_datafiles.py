ca6421faed485448eddebf361ca235e15a46e6ea
wcarruthers <wcarruthers92@gmail.com>
2021-05-26 08:51:18 -0500

    Update 13F_parse_datafiles.py
    fixed parsing data into dataframe and tested

--- data_acq/13F_parse_datafiles.py
+++ data_acq/13F_parse_datafiles.py
@@ -16,10 +16,13 @@ def parse_13f_file(file, CIK):
     
     try:
         date = datetime.datetime.strptime(soup.find('signaturedate').contents[0], '%m-%d-%Y')
-        print(date)
+        #print(date)
         name = soup.find('name').contents[0]
+        #print(name)
     except:
-        print("failed date")
+        #old files are not in the xml as expected
+        #print(file)
+        #print("failed date")
         return
         
     cols = ['nameOfIssuer', 'cusip', 'value', 'sshPrnamt', 'sshPrnamtType', 'putCall']
@@ -29,44 +32,39 @@ def parse_13f_file(file, CIK):
     for infotable in soup.find_all(['ns1:infotable', 'infotable']):
         row = []
         for col in cols:
+            
             data = infotable.find([col.lower(), 'ns1:' + col.lower()])
+            
             if data is not None:
                 #print(data)
                 row.append(data.text.strip())
             else:
                 row.append('NaN')
-            row.append(date)
-            row.append(CIK)
-            row.append(name)
-            d.append(row)
-            #print(d)
-        df = pd.DataFrame(d)
-        print(df.head())
-        cols.append('date')
-        cols.append('fund_cik')
-        cols.append('fund')
+        row.append(date)
+        row.append(CIK)
+        row.append(name)
+        d.append(row)
+            
+    df = pd.DataFrame(d)
+    cols.append('date')
+    cols.append('fund_cik')
+    cols.append('fund')
         #print(df.columns)
         #print(cols)
-        try:
-            df.columns = cols
-            print(df.head())
-        except:
-            print("COLUMN ERROR")
-            return
-        print(df.head())
-        
+    try:
+        df.columns = cols
         return df
+    except:
+        print("COLUMN ERROR")
+        return
 
-def parse_cik_files(CIK, f):
-    #files = os.listdir(os.path.join('data', 'sec-edgar-filings', CIK, '13F-HR'))
-    files = []
-    files.append(f)
+
+def parse_cik_files(CIK):
+    files = os.listdir(os.path.join('data', 'sec-edgar-filings', CIK, '13F-HR'))
     df = pd.DataFrame()
     for f in sorted(files):
         data = parse_13f_file(f, CIK)
-        print(data)
-        df.append(data)
-        print(df)
+        df = df.append(data)
     return df
 
 def get_ciks(cik_path):
@@ -83,14 +81,19 @@ def get_ciks(cik_path):
     return df.CIK.values
 
 if __name__ == "__main__":
-    #cik_path = os.path.join('data', 'cik_file.txt')
-    #ciks = get_ciks(cik_path)
-    #df = pd.DataFrame()
-    #for c in ciks:
-     #   df_c = parse_cik_files(c)
-     #   print(df_c.head())
-     #   df = df.append(df_c)
+    cik_path = os.path.join('data', 'cik_file.txt')
+    ciks = get_ciks(cik_path)
+    df = pd.DataFrame()
+    for c in ciks:
+        df_c = parse_cik_files(c)
+        print(df_c.head())
+        df = df.append(df_c)
+    print(df.head())
+    df['value'] = df['value'].astype(float)
+    print(df.groupby(['fund','date'])['value'].sum())
+    
+    #for testing
+    #df = parse_cik_files('0000089014', '0001567619-21-001894')
     #print(df.head())
     #df['value'] = df['value'].astype(float)
     #print(df.groupby(['fund','date'])['value'].sum())
-    parse_cik_files('0000089014', '0001567619-21-001894')
